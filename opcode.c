e359f56fc33e1b43469444dc0c1251b17f4e2de5
py-cc <paolacarrero731@gmail.com>
2021-05-13 22:55:55 -0500

    fix style betty

--- opcode.c
+++ opcode.c
@@ -118,14 +118,14 @@ void fun_pop(stack_t **head, unsigned int line_number)
 	stack_t *tmp = NULL;
 
 	if (head == NULL || *head == NULL)
-        {
-                fprintf(stderr, "L%d: usage: can't pop an empty stack\n", line_number);
+	{
+		fprintf(stderr, "L%d: usage: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
-                return;
+		return;
 	}
-        tmp = *head;
-        *head = (*head)->next;
-        free(tmp);
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
 }
 
 /**
@@ -140,14 +140,14 @@ void fun_swap(stack_t **head, unsigned int line_number)
 	int i = 0;
 
 	tmp = *head;
- 
-        if (*head == NULL || head == NULL)
-        {
-                fprintf(stderr, "L%d: usage: can't swap, stack null\n", line_number);
-                exit(EXIT_FAILURE);
-                return;
-        }
-	
+
+	if (*head == NULL || head == NULL)
+	{
+		fprintf(stderr, "L%d: usage: can't swap, stack null\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
+
 	while (tmp != NULL)
 	{
 		tmp = (tmp)->next;
@@ -156,8 +156,8 @@ void fun_swap(stack_t **head, unsigned int line_number)
 	if (i < 2)
 	{
 		fprintf(stderr, "L%d: usage: can't swap, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-                return;
+		exit(EXIT_FAILURE);
+		return;
 	}
 	tmp2 = (*head)->next;
 	(*head)->next = (*head)->next->next;
@@ -174,35 +174,35 @@ void fun_swap(stack_t **head, unsigned int line_number)
 void fun_add(stack_t **head, unsigned int line_number)
 {
 	stack_t *tmp = NULL, *tmp2 = NULL;
-        int i = 0;
+	int i = 0;
 	int sum = 0;
 
 	tmp = *head;
- 
-        if (*head == NULL || head == NULL)
-        {
+
+	if (*head == NULL || head == NULL)
+	{
 		fprintf(stderr, "L%d: usage: can't add, stack null\n", line_number);
 		exit(EXIT_FAILURE);
-                return;
-        }
-        
+		return;
+	}
+
 	while (tmp != NULL)
-        {
+	{
 		tmp = (tmp)->next;
-                i++;
-        }
-        if (i < 2)
-        {
-                fprintf(stderr, "L%d: usage: can't add, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-                return;
-        }
+		i++;
+	}
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: usage: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
 	sum = (*head)->n + (*head)->next->n;
 
-        tmp2 = *head;
+	tmp2 = *head;
 	*head = (*head)->next;
 	(*head)->n = sum;
-        free(tmp2);
+	free(tmp2);
 }
 
 /**

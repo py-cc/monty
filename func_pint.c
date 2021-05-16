e45fbeea1741db102a5fcd856e00b3b0d109e2ef
py-cc <paolacarrero731@gmail.com>
2021-05-16 13:52:52 -0500

    fix condictional

--- func_pint.c
+++ func_pint.c
@@ -1,21 +1,18 @@
 #include "monty.h"
-
+/**
+ * func_pint -  opcode pint prints the value at the top of the stack
+ * @head: pointer to double linked list
+ * @line_number: line file to check
+ *
+ *
+ */
 void func_pint(stack_t **head, unsigned int line_number)
 {
-	stack_t *tmp;
-
-	tmp = *head;
-	if (*head == NULL)
+	if (*head)
+		printf("%d\n", (*head)->n);
+	else
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
-	while (tmp != NULL)
-	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-	}
-	*head = tmp;
-
 }
